4cd135c971d7256ab00001d284114ecd093ca72c
iv@zaoblako.com <i.a.volik@gmail.com>
2021-02-28 23:06:55 +0300

    initial commit

---
+++ cars4rent/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cars4rent project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cars4rent.settings')
+
+application = get_asgi_application()
