bcd30cd0310f049abee927aa1a377d464c877d6c
iv@zaoblako.com <i.a.volik@gmail.com>
2021-03-03 11:19:48 +0300

    styles.css added

--- cars4rent/settings.py
+++ cars4rent/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.joinpath('static')
-
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = BASE_DIR.joinpath('static')
+STATIC_URL = '/static/'
 
